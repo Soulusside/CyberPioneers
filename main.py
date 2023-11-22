d6662e38019ae51ea8a5970cae3fe8c04449b247
Soulus <Juluur93@gmail.com>
2023-11-22 10:31:11 +0900

    init

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 
 class ProductApp(App):
     def build(self):
-        self.products = []
-        layout = BoxLayout(orientation='vertical')
-        self.label = Label(text='Продукты:')
-        layout.add_widget(self.label)
-        self.product_list = Label(text='\n'.join(self.products))
-        layout.add_widget(self.product_list)
-        self.text_input = TextInput(hint_text='Введите продукт')
-        layout.add_widget(self.text_input)
-        btn_add = Button(text='Добавить продукт', on_press=self.add_product)
-        layout.add_widget(btn_add)
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=10)
+
+        my_products_button = Button(text='Мои товары', size_hint=(None, None))
+        my_products_button.bind(on_press=self.My_products)
+        layout.add_widget(my_products_button)
+
+        search_products_button = Button(text='Поиск товаров', size_hint=(None, None))
+        search_products_button.bind(on_press=self.Search_products)
+        layout.add_widget(search_products_button)
+
         return layout
 
-    def add_product(self, instance):
-        product_name = self.text_input.text.strip()  # Удаляем пробелы в начале и конце строки
-        if product_name:
-            self.products.append(product_name)
-            self.product_list.text = '\n'.join(self.products)
-            self.text_input.text = ''
+    def My_products(self, instance):
+        pass
+
+    def Search_products(self, instance):
+        pass
 
 if __name__ == '__main__':
     ProductApp().run()
 
-print()
-ds
